d9bc4a25c042428056d78023ae829270147023b1
ayslan.batista@hotmail.com <ayslan.batista@hotmail.com>
2024-03-21 18:22:01 +0000

    Dia6 81%

---
+++ dia06/ROW_NUMBER.sql
@@ -0,0 +1,45 @@
+-- Databricks notebook source
+WITH tb_rn AS (
+  SELECT
+    idProduto,
+    descCategoria,
+    vlPesoGramas,
+    row_number() OVER (
+      PARTITION BY descCategoria
+      ORDER BY
+        vlPesoGramas DESC
+    ) AS rnProduto
+  FROM
+    silver.olist.produto
+  WHERE
+    descCategoria IS NOT NULL
+)
+SELECT
+  *
+FROM
+  tb_rn
+
+WHERE rnProduto <= 5
+
+-- COMMAND ----------
+
+WITH tb_rn AS (
+  SELECT
+    *,
+    row_number() OVER (
+      PARTITION BY descCategoria
+      ORDER BY
+        vlComprimentoCm * vlAlturaCm * vlLarguraCm DESC
+    ) AS rnProduto,
+    vlComprimentoCm * vlAlturaCm * vlLarguraCm AS volumeProduto
+  FROM
+    silver.olist.produto
+  WHERE
+    descCategoria IS NOT NULL
+)
+SELECT
+  *
+FROM
+  tb_rn
+
+WHERE rnProduto = 1
